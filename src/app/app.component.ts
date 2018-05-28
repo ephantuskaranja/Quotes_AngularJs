8c299970ef7857879ddd915b9957d3fb7deeccc3
Ephantus karanja <ephantusslogan@gmail.com>
2018-05-28 11:30:48 +0300

    add constructor to add content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Quotes';
+    Quote = [
+        new Quote(1,'all of me', `epha`, `ephantus`),
+        new Quote(2,'what a man', `cages`, `ephantus`),
+        new Quote(3,'all of business', `timephy`, `ephantus`),
+    ]
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,3 @@
 export class Quote {
+    constructor(public id: number, public quote: string, public author: string, public postedBy: string) { }
 }
